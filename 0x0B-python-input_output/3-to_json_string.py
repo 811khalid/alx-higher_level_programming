8ca8e099b7ec3df74e587b6531ced7ffe42e91c7
811khalid <khalidtewfiq151@gmail.com>
2024-01-09 05:04:51 -0800

    first add

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Main doc
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function doc
+    """
+    return json.dumps(my_obj)
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Main doc
+"""
+
+
+def class_to_json(obj):
+    """
+    fun doc
+    """
+    return obj.__dict__
