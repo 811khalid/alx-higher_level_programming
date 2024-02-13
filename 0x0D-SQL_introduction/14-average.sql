489983ba9ae173fba16de5dfec53c49a40e284d4
811khalid <khalidtewfiq151@gmail.com>
2024-02-13 04:55:33 -0800

    update

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
--- Write a script that removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0 in your MySQL server.
-DELETE FROM second_table
-WHERE score <= 5;
+-- Write a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
+SELECT AVG(score) as average
+FROM second_table;
